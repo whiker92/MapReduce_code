59c8b39a96c567997247b08fb16e402efcb8eeaa
whiker92 <349110588@qq.com>
2016-07-07 07:42:52 -0700

    add multiLinearReg

--- apriori/apriori_final.java
+++ apriori/apriori_final.java
@@ -1,13 +1,10 @@
 
-/*
+/**
  * 
  *  Apriori Algorithm: http://www.codeproject.com/KB/recipes/AprioriAlgorithm.aspx
  * 
- *  支持度5 频繁集于out2中
- * 
- *  Note:
- *  ======
- *  Running the final code with log4j causes performance hit. Disable log4j in the final run
+ * 输入数据：Item1, Item2, Item3...
+ * 输出数据：{Frequent Items}, support value (in file out2)
  * 
  */
 
@@ -53,18 +50,12 @@ public class apriori {
 			String line = value.toString();
 			StringTokenizer tokenizer = new StringTokenizer(line);
 
-			Logger logger = Logger.getLogger(apriori.class.getName());
-			PropertyConfigurator.configure("/home/whiker/soft/hadoop-1.2.1/conf/log4j.properties");
-			logger.info("Map: Line is => " + line);
-
-			// Word Count
 			while (tokenizer.hasMoreTokens()) {
 				word.set(tokenizer.nextToken());
 				count.set(Integer.toString(1));
 				try {
 					context.write(word, count);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
@@ -75,28 +66,17 @@ public class apriori {
 	public static class CandidateGenMap extends Mapper<LongWritable, Text, Text, Text> {
 		private Text word = new Text();
 		private Text count = new Text();
-		private int Support = 5;
 
 		public void CandidatesGenRecursion(Vector<String> in, Vector<String> out, int length, int level, int start,
 				Context context) throws IOException {
 
-			// StringBuffer current = new StringBuffer();
-			Logger logger = Logger.getLogger(apriori.class.getName());
-			PropertyConfigurator.configure("/home/whiker/soft/hadoop-1.2.1/conf/log4j.properties");
 			int i, size;
-
-			logger.info("CandidatesGenRecursion: [len, lev, start] => [" + length + "," + level + "," + start + "]");
 
 			for (i = start; i < length; i++) {
 				if (level == 0) {
 					out.add(in.get(i));
-					logger.info("CandidatesGenRecursion: Candidate Level0 is => " + out.toString());
 				} else {
-					logger.info("CandidatesGenRecursion: Level0+ Before " + out.toString());
-					logger.info("CandidatesGenRecursion: Level0++ Before " + out.toString() + "-" + in.get(i));
 					out.add(in.get(i));
-					// Emit this
-					logger.info("CandidatesGenRecursion: Candidate Level" + level + " is => " + out.toString());
 
 					int init = 1;
 					StringBuffer current = new StringBuffer();
@@ -110,14 +90,11 @@ public class apriori {
 						}
 					}
 
-					logger.info("CandidatesGenRecursion: EMIT => " + current.toString());
-
 					word.set(current.toString());
 					count.set(Integer.toString(1));
 					try {
 						context.write(word, count);
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
@@ -134,13 +111,11 @@ public class apriori {
 
 		@Override
 		public void map(LongWritable key, Text value, Context context) throws IOException {
+			Configuration conf = context.getConfiguration();
+			int Support = Integer.parseInt(conf.get("Support"));
+			
 			String line = value.toString();
 			StringTokenizer tokenizer = new StringTokenizer(line);
-
-			Logger logger = Logger.getLogger(apriori.class.getName());
-			PropertyConfigurator.configure("/home/whiker/soft/hadoop-1.2.1/conf/log4j.properties");
-			logger.info("CandidateGenMap: Line is => " + line);
-			logger.info("CandidateGenMap: KEY is => " + key.toString());
 
 			Configuration config = HBaseConfiguration.create();
 			HTable tableC = new HTable(config, "FreqItems");
@@ -149,24 +124,12 @@ public class apriori {
 			while (tokenizer.hasMoreTokens()) {
 
 				String str = tokenizer.nextToken();
-				logger.info("CandidateGenMap: BEFORE LOOP Str is " + str);
-
-				// Now, to retrieve the data we just wrote. The values that come
-				// back are
-				// Result instances. Generally, a Result is an object that will
-				// package up
-				// the hbase return into the form you find most palatable.
 				Get g = new Get(Bytes.toBytes(str));
 				Result r = tableC.get(g);
 				byte[] contents = r.getValue(Bytes.toBytes("count"), Bytes.toBytes(str));
 
-				// If we convert the value bytes, we should get back 'Some
-				// Value', the
-				// value we inserted at this location.
 				String countStr = Bytes.toString(contents);
-				logger.info("CandidateGenMap: BEFORE LOOP Count-Str is " + countStr);
 
-				// Prune
 				int cnt = Integer.parseInt(countStr);
 				if (cnt >= Support) {
 					lst.add(str);
@@ -175,21 +138,17 @@ public class apriori {
 
 			}
 
-			logger.info("CandidateGenMap: Line is => " + line);
-			logger.info("APRIORI_Map: BEFORE EMIT =>" + lst.toString());
-
 			Vector<String> combinations = new Vector<String>();
 
 			if (!lst.isEmpty()) {
 				CandidatesGenRecursion(lst, combinations, loop, 0, 0, context);
 			}
-			for(String i:lst){
+			for (String i : lst) {
 				word.set(i.toString());
 				count.set(Integer.toString(1));
 				try {
 					context.write(word, count);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
@@ -201,10 +160,6 @@ public class apriori {
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException {
 			Iterator<Text> iterator = values.iterator();
 			int sum = 0;
-
-			Logger logger = Logger.getLogger(apriori.class.getName());
-			PropertyConfigurator.configure("/home/whiker/soft/hadoop-1.2.1/conf/log4j.properties");
-			logger.info("FrequentItemsReduce: ");
 
 			Configuration config = HBaseConfiguration.create();
 			HBaseAdmin hBaseAdmin = new HBaseAdmin(config);
@@ -222,14 +177,10 @@ public class apriori {
 			}
 
 			try {
-				logger.info("FrequentItemsReduce: Adding to DB FreqItems (Key,Qualifier,Value) => (" + key.toString()
-						+ "," + key.toString() + "," + Integer.toString(sum) + ")");
 				context.write(key, new Text(Integer.toString(sum)));
 				p.add(Bytes.toBytes("count"), Bytes.toBytes(key.toString()), Bytes.toBytes(Integer.toString(sum)));
 				context.progress();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				logger.error("FrequentItemsReduce: Error while setting sum = " + sum);
 				e.printStackTrace();
 			}
 			table.put(p);
@@ -237,16 +188,14 @@ public class apriori {
 	}
 
 	public static class CandidateGenReduce extends Reducer<Text, Text, Text, Text> {
-		private int Support = 5;
 
 		@Override
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException {
+			Configuration conf = context.getConfiguration();
+			int Support = Integer.parseInt(conf.get("Support"));
+			
 			Iterator<Text> iterator = values.iterator();
 			int sum = 0;
-
-			Logger logger = Logger.getLogger(apriori.class.getName());
-			PropertyConfigurator.configure("/home/whiker/soft/hadoop-1.2.1/conf/log4j.properties");
-			// logger.info("CandidateGenReduce: ");
 
 			Configuration config = HBaseConfiguration.create();
 			HTable table = new HTable(config, "FreqItems");
@@ -258,13 +207,10 @@ public class apriori {
 			}
 			if (sum >= Support) {
 				try {
-					logger.info("CandidateGenReduce: Adding to DB Candidates (Key,Qualifier,Value) => ("
-							+ key.toString() + "," + key.toString() + "," + Integer.toString(sum) + ")");
 					context.write(key, new Text(Integer.toString(sum)));
 					p.add(Bytes.toBytes("count"), Bytes.toBytes(key.toString()), Bytes.toBytes(Integer.toString(sum)));
 					context.progress();
 				} catch (InterruptedException e) {
-					logger.error("CandidateGenReduce: Error while setting sum = " + sum);
 					e.printStackTrace();
 				}
 				table.put(p);
@@ -274,25 +220,22 @@ public class apriori {
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		System.out.println("Started Job");
+		conf.set("Support", "5");
 		Date dt;
-		long start, end; // Start and end time
-
-		// Start Timer
+		long start, end;
 		dt = new Date();
 		start = dt.getTime();
 
-		args[1] = "hdfs://localhost:9000/" + start + "/output_apriori/out1";
-		args[2] = "hdfs://localhost:9000/" + start + "/output_apriori/out2";
+		args[1] = "hdfs://localhost:9000/output_apriori/" + start + "/out1";
+		args[2] = "hdfs://localhost:9000/output_apriori/" + start + "/out2";
 
 		Configuration config = HBaseConfiguration.create();
 		HBaseAdmin hBaseAdmin = new HBaseAdmin(config);
-		if (hBaseAdmin.tableExists("FreqItems")) {// 如果存在要创建的表，那么先删除，再创建
+		if (hBaseAdmin.tableExists("FreqItems")) {
 			hBaseAdmin.disableTable("FreqItems");
 			hBaseAdmin.deleteTable("FreqItems");
 			System.out.println("In main: FreqItems is exist,detele....");
 		}
-
 		Job job = new Job(conf, "apriori freq items");
 		job.setJarByClass(apriori.class);
 		job.setMapperClass(FrequentItemsMap.class);
@@ -305,27 +248,20 @@ public class apriori {
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 
-		Logger logger = Logger.getLogger(apriori.class.getName());
-		PropertyConfigurator.configure("/home/whiker/soft/hadoop-1.2.1/conf/log4j.properties");
-		logger.info("Entering application Apriori.");
-
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-		// System.exit(job.waitForCompletion(true) ? 0 :1);
 		if (job.waitForCompletion(true)) {
-			logger.info("Completed Word Count Generation SUCCESSFULLY for Apriori.");
-		} else {
-			logger.info(" ERROR - Completed Candidate Generation for Apriori.");
+			System.out.println("FrequentItem complete.");
 		}
 
 		Configuration conf1 = new Configuration();
-		System.out.println("Starting Job2");
+		conf1.set("Support", "5");
 		Job job2 = new Job(conf1, "apriori candidate gen");
 		job2.setJarByClass(apriori.class);
 
 		job2.setMapperClass(CandidateGenMap.class);
-		job2.setCombinerClass(CandidateGenReduce.class); //
+		job2.setCombinerClass(CandidateGenReduce.class); 
 		job2.setReducerClass(CandidateGenReduce.class);
 		job2.setMapOutputKeyClass(Text.class);
 		job2.setMapOutputValueClass(Text.class);
@@ -339,21 +275,11 @@ public class apriori {
 		FileOutputFormat.setOutputPath(job2, new Path(args[2]));
 
 		if (job2.waitForCompletion(true)) {
-			logger.info("Completed Candidate Generation SUCCESSFULLY for Apriori 2.");
-
-		} else {
-			logger.info(" ERROR - Completed Candidate Generation for Apriori 2.");
+			System.out.println("CandidateGen complete.");
 		}
 
-		// End Timer
 		dt = new Date();
 		end = dt.getTime();
-
-		logger.info("Completed Association Rule for Apriori.");
-		logger.info("Association Rule Start time=>" + start);
-		logger.info("Association Rule End time=>" + end);
-
-		logger.info("Total time => " + (end - start));
 
 	}
 
---
+++ multilinereg/multilinereg.java
@@ -0,0 +1,142 @@
+
+/**
+ *  输入格式：y x1 x2...
+ *  输出格式：theta0, theta1, theta2...
+ *  主要思想：
+		在Mapper中进行SGD：theta(j) = theta(j)+alpha*(y-y')*x(j)
+		在Reducer中进行加和，取平均值。
+	缺点：
+		SGD刚开始时的theta值偏差较大，因此取平均值得到的theta，对于样本数比较小的情况下偏差大。
+ */
+
+import java.io.IOException;
+import java.util.*;
+import java.lang.String;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class multilinereg {
+
+	public static class MultiLineRegMap extends Mapper<LongWritable, Text, Text, Text> {
+		private Text k = new Text();
+		private Text val = new Text();
+		private static Vector<Double> thetavec = new Vector<Double>();
+
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+			Configuration conf = context.getConfiguration();
+			String alpha = conf.get("alpha");
+			String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line);
+
+			int first = 0;
+			int y = 0;
+			Vector<Double> x = new Vector<Double>();
+			Double h = 0d;
+			String values = "";
+
+			while (tokenizer.hasMoreTokens()) {
+				if (first == 0) {
+					y = Integer.parseInt(tokenizer.nextToken());
+					x.addElement(1.0D);
+				} else {
+					x.addElement(Double.parseDouble(tokenizer.nextToken()));
+				}
+				first++;
+			}
+			if (thetavec.isEmpty()) {
+				for (int i = 0; i < first; i++) {
+					thetavec.addElement(0.0D);
+				}
+			}
+			for (int i = 0; i < first; i++) {
+				h += x.get(i) * thetavec.get(i);
+			}
+			for (int i = 0; i < first; i++) {
+				Double theta = thetavec.get(i) + Double.parseDouble(alpha) * (y - h) * x.get(i);
+				thetavec.set(i, theta);
+				if (i == 0) {
+					values += theta.toString();
+				} else {
+					values += " " + theta.toString();
+				}
+			}
+			k.set("1");
+			val.set(values);
+			context.write(k, val);
+		}
+	}
+
+	public static class MultiLineRegReduce extends Reducer<Text, Text, Text, Text> {
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+			Iterator<Text> iterator = values.iterator();
+			Vector<Double> thetavec = new Vector<Double>();
+			Double num = 0D;
+
+			while (iterator.hasNext()) {
+				String bstr = iterator.next().toString();
+				if (!bstr.isEmpty()) {
+					String[] vals = bstr.split(" ");
+					if (thetavec.isEmpty()) {
+						for (String i : vals) {
+							thetavec.addElement(Double.parseDouble(i));
+						}
+					} else {
+						for (int i = 0; i < vals.length; i++) {
+							Double theta = thetavec.get(i) + Double.parseDouble(vals[i]);
+							thetavec.set(i, theta);
+						}
+					}
+					num++;
+				}
+			}
+
+			String ans = "";
+			for (int i = 0; i < thetavec.size(); i++) {
+				if (i == 0) {
+					Double theta = thetavec.get(i) / num;
+					ans += theta.toString();
+				} else {
+					Double theta = thetavec.get(i) / num;
+					ans += " " + theta.toString();
+				}
+			}
+			context.write(new Text(ans), new Text(""));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("alpha", "0.03");
+
+		args[1] = "hdfs://localhost:9000/output_multilinereg/" + new Date().getTime() + "/out";
+		Job job = new Job(conf, "multi_linear_reg");
+		job.setJarByClass(multilinereg.class);
+		job.setMapperClass(MultiLineRegMap.class);
+		job.setReducerClass(MultiLineRegReduce.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		if (job.waitForCompletion(true)) {
+			System.out.println("Completed multi-linear-regression.");
+		}
+	}
+}
