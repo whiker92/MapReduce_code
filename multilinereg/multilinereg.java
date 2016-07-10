0a27174f1abdb7eaa8d7b796793d0a5b01842b1f
whiker92 <349110588@qq.com>
2016-07-10 05:44:22 -0700

    add accuracy&&numIter

--- multilinereg/multilinereg.java
+++ multilinereg/multilinereg.java
@@ -4,16 +4,22 @@
  *  输出格式：theta0, theta1, theta2...
  *  主要思想：
 		在Mapper中进行SGD：theta(j) = theta(j)+alpha*(y-y')*x(j)
-		在Reducer中进行加和，取平均值。
-    缺点：
-		SGD刚开始时的theta值偏差较大，因此取平均值得到的theta，对于样本数比较小的情况下偏差大。
+		在Reducer中进行加和，取平均值
+		对每次计算结果与y值进行作差，查看准确度，若达到要求，停止迭代continue=true stop=false
+ *  缺点：
+		SGD刚开始时的theta值偏差较大，因此取平均值得到的theta，对于样本数比较小的情况下偏差大
  */
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 import java.lang.String;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -36,18 +42,22 @@ public class multilinereg {
 
 			Configuration conf = context.getConfiguration();
 			String alpha = conf.get("alpha");
+			Double accuracy = Double.parseDouble(conf.get("accuracy"));
+			String thetaPath = conf.get("thetaPath");
+
 			String line = value.toString();
 			StringTokenizer tokenizer = new StringTokenizer(line);
 
-			int first = 0;
-			int y = 0;
-			Vector<Double> x = new Vector<Double>();
-			Double h = 0d;
-			String values = "";
+			int first = 0; // number of feature
+			Double y = 0.0; // value of y
+			Vector<Double> x = new Vector<Double>(); // value list of xi
+			Double h = 0.0; // value of y'
+			String values = ""; // value of theta to reducer
+			Double tol = 0.0; // value of y-y'
 
 			while (tokenizer.hasMoreTokens()) {
 				if (first == 0) {
-					y = Integer.parseInt(tokenizer.nextToken());
+					y = Double.parseDouble(tokenizer.nextToken());
 					x.addElement(1.0D);
 				} else {
 					x.addElement(Double.parseDouble(tokenizer.nextToken()));
@@ -55,20 +65,30 @@ public class multilinereg {
 				first++;
 			}
 			if (thetavec.isEmpty()) {
+				FileSystem hdfs = FileSystem.get(conf);
+				FSDataInputStream in = hdfs.open(new Path(thetaPath));
+				BufferedReader d = new BufferedReader(new InputStreamReader(in));
+				String[] thetaString = d.readLine().split(" ");
 				for (int i = 0; i < first; i++) {
-					thetavec.addElement(0.0D);
+					thetavec.addElement(Double.parseDouble(thetaString[i]));
 				}
 			}
 			for (int i = 0; i < first; i++) {
 				h += x.get(i) * thetavec.get(i);
 			}
-			for (int i = 0; i < first; i++) {
-				Double theta = thetavec.get(i) + Double.parseDouble(alpha) * (y - h) * x.get(i);
-				thetavec.set(i, theta);
-				if (i == 0) {
-					values += theta.toString();
-				} else {
+			// the condition of stop iterator: continue=true stop=false
+			tol = Math.abs(h - y);
+			if (tol > accuracy) {
+				values += String.valueOf(true);
+				for (int i = 0; i < first; i++) {
+					Double theta = thetavec.get(i) + Double.parseDouble(alpha) * ((y - h) * x.get(i));
+					thetavec.set(i, theta);
 					values += " " + theta.toString();
+				}
+			} else {
+				values += String.valueOf(false);
+				for (int i = 0; i < first; i++) {
+					values += " " + thetavec.get(i).toString();
 				}
 			}
 			k.set("1");
@@ -84,59 +104,117 @@ public class multilinereg {
 			Vector<Double> thetavec = new Vector<Double>();
 			Double num = 0D;
 
+			boolean stopOrNot = true;
 			while (iterator.hasNext()) {
+				boolean first = true;
 				String bstr = iterator.next().toString();
-				if (!bstr.isEmpty()) {
-					String[] vals = bstr.split(" ");
-					if (thetavec.isEmpty()) {
-						for (String i : vals) {
+				String[] vals = bstr.split(" ");
+				if (thetavec.isEmpty()) {
+					for (String i : vals) {
+						if (first) {
+							first = false;
+							stopOrNot = stopOrNot && Boolean.parseBoolean(i);
+						} else
 							thetavec.addElement(Double.parseDouble(i));
-						}
-					} else {
-						for (int i = 0; i < vals.length; i++) {
-							Double theta = thetavec.get(i) + Double.parseDouble(vals[i]);
-							thetavec.set(i, theta);
+					}
+				} else {
+					for (int i = 0; i < vals.length; i++) {
+						if (first) {
+							first = false;
+							stopOrNot = stopOrNot && Boolean.parseBoolean(vals[i]);
+						} else {
+							Double theta = thetavec.get(i - 1) + Double.parseDouble(vals[i]);
+							thetavec.set(i - 1, theta);
 						}
 					}
-					num++;
 				}
+				num++;
 			}
 
-			String ans = "";
+			String ans = String.valueOf(stopOrNot);
 			for (int i = 0; i < thetavec.size(); i++) {
-				if (i == 0) {
-					Double theta = thetavec.get(i) / num;
-					ans += theta.toString();
-				} else {
-					Double theta = thetavec.get(i) / num;
-					ans += " " + theta.toString();
-				}
+				Double theta = thetavec.get(i) / num;
+				ans += " " + theta.toString();
 			}
 			context.write(new Text(ans), new Text(""));
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
+		// 需要给定的值：
+		// 特征个数，迭代次数，准确度，学习率，hdfs地址
+		int numIter = 50, numFeature = 1 + 1;
+		long start = new Date().getTime();
 		Configuration conf = new Configuration();
-		conf.set("alpha", "0.03");
+		conf.set("alpha", "0.03"); // 学习率
+		conf.set("accuracy", "0.01"); // 误差范围
+		conf.set("numIter", "2"); // 最大迭代次数
+		conf.set("numFeature", "1");// 特征个数
+		conf.set("thetaPath", "hdfs://localhost:9000/multilineregtheta/" + String.valueOf(start) + "/theta.txt");
 
-		args[1] = "hdfs://localhost:9000/output_multilinereg/" + new Date().getTime() + "/out";
-		Job job = new Job(conf, "multi_linear_reg");
-		job.setJarByClass(multilinereg.class);
-		job.setMapperClass(MultiLineRegMap.class);
-		job.setReducerClass(MultiLineRegReduce.class);
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(Text.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
+		FileSystem hdfs = FileSystem.get(conf);
 
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		for (int iter = 0; iter < numIter; iter++) {
 
-		if (job.waitForCompletion(true)) {
-			System.out.println("Completed multi-linear-regression.");
+			// theta list file
+			if (iter == 0) {
+				String theta = "0";
+				for (int i = 1; i < numFeature; i++)
+					theta += " 0";
+				hdfs.mkdirs(new Path("hdfs://localhost:9000/multilineregtheta/"));
+				hdfs.mkdirs(new Path("hdfs://localhost:9000/multilineregtheta/" + String.valueOf(start)));
+				FSDataOutputStream outputStream = hdfs.create(
+						new Path("hdfs://localhost:9000/multilineregtheta/" + String.valueOf(start) + "/theta.txt"));
+				outputStream.write(theta.getBytes(), 0, theta.getBytes().length);
+				outputStream.close();
+			}
+
+			args[1] = "hdfs://localhost:9000/output_multilinereg/" + String.valueOf(start) + "/out";
+			Job job = new Job(conf, "multi_linear_reg");
+			job.setJarByClass(multilinereg.class);
+			job.setMapperClass(MultiLineRegMap.class);
+			job.setReducerClass(MultiLineRegReduce.class);
+			job.setMapOutputKeyClass(Text.class);
+			job.setMapOutputValueClass(Text.class);
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(Text.class);
+			job.setInputFormatClass(TextInputFormat.class);
+			job.setOutputFormatClass(TextOutputFormat.class);
+
+			FileInputFormat.addInputPath(job, new Path(args[0]));
+			FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+			if (job.waitForCompletion(true)) {
+				System.out.println("Completed iterator " + String.valueOf(iter) + " multi-linear-regression.");
+			}
+
+			// find condition value of iterator
+			FSDataInputStream in = hdfs.open(new Path(args[1] + "/part-r-00000"));
+			BufferedReader d = new BufferedReader(new InputStreamReader(in));
+			String[] values = d.readLine().split(" ");
+			int numTheta = values.length;
+			String ans = values[1];
+			for (int iTheta = 2; iTheta < numTheta; iTheta++) {
+				ans += " " + values[iTheta];
+			}
+			d.close();
+			hdfs.delete(new Path(args[1]), true);
+			if (values[0].equals("false") || iter == numIter - 1) { // stop
+																	// iterator
+				hdfs.mkdirs(new Path(args[1]));
+				FSDataOutputStream outputStream = hdfs.create(new Path(args[1] + "/theta"));
+				outputStream.write(ans.getBytes(), 0, ans.getBytes().length);
+				outputStream.close();
+				break;
+			} else { // continue iterator
+				hdfs.delete(new Path("hdfs://localhost:9000/multilineregtheta/" + String.valueOf(start)), true);
+				hdfs.mkdirs(new Path("hdfs://localhost:9000/multilineregtheta/" + String.valueOf(start)));
+				FSDataOutputStream outputStream = hdfs.create(
+						new Path("hdfs://localhost:9000/multilineregtheta/" + String.valueOf(start) + "/theta.txt"));
+				outputStream.write(ans.getBytes(), 0, ans.getBytes().length);
+				outputStream.close();
+			}
 		}
+		hdfs.close();
 	}
 }
